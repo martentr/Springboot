032537f5457c3692d5c8a7d6975250ca46bfe9ec
martentr <55134911+martentr@users.noreply.github.com>
2023-10-05 14:20:34 +0300

    Update CustomUserDetails.java

--- src/main/java/com/App/security/CustomUserDetails.java
+++ src/main/java/com/App/security/CustomUserDetails.java
@@ -23,7 +23,6 @@ public class CustomUserDetails implements UserDetails{
 public Collection<? extends GrantedAuthority> getAuthorities() {
     // Implement to return the user's authorities/roles
     // You can load roles from your database here
-    // Example: return user.getRoles();
     return user.getRoles().stream()
         .map(role -> new SimpleGrantedAuthority(role.getName()))
         .collect(Collectors.toList());
@@ -42,30 +41,23 @@ public Collection<? extends GrantedAuthority> getAuthorities() {
 
     @Override
     public boolean isAccountNonExpired() {
-        // Implement account expiration logic here
         return true; // Return true if the account is not expired
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        // Implement account locking logic here
         return true; // Return true if the account is not locked
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        // Implement credentials expiration logic here
         return true; // Return true if the credentials are not expired
     }
 
     @Override
     public boolean isEnabled() {
-        // Implement account enabled/disabled logic here
         return true; // Return true if the account is enabled
     }
 
-    // Implement other UserDetails methods (isEnabled, isAccountNonExpired, etc.) as needed
-
-    // Additional methods and getters/setters for custom user details
 }
 
